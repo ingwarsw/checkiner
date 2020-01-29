dd7cfc8d72a5f686763650ef83b7b37227e7ac01
Karol Lassak <ingwar@ingwar.eu.org>
2020-01-29 21:29:00 +0000

    Switch to puppeter

--- build.js
+++ build.js
@@ -1,59 +1,50 @@
-const { Chromeless } = require('chromeless')
+const puppeteer = require('puppeteer')
 const getUniqueRandomArray = require("get-unique-random-array").default
 const randomInt = require('random-int');
 
 const username = "rawgni@ingwar.eu.org"
 const password = process.env.CROSS_PASS
 
-const chromeless = new Chromeless({
-    debug: true,
-    implicitWait: true,
-    waitTimeout: 20000,
-    launchChrome: true
-})
-
 async function run() {
-    console.log("Running")
-    await chromeless
-        .goto('https://app.crossover.com/x/login')
-        .wait('input[type="email"]')
-        .goto('https://app.crossover.com/x/login')
-        .type(username, 'input[type="email"]')
-        .type(password, 'input[type="password"]')
-        .click('button[type="submit"]')
-        .wait('.checkin-chat')
-        .goto('https://app.crossover.com/x/dashboard/contractor/checkin-chat')
+    const browser = await puppeteer.launch({
+        headless: true,
+        env: {
+            TZ: 'Europe/Warsaw',
+        }
+    })
+    try {
+        console.log("Running")
+        const page = await browser.newPage();
+        await page.goto('https://app.crossover.com/x/login')
+        await page.waitForSelector('input[type="email"]')
+        await page.goto('https://app.crossover.com/x/login')
+        await page.type('input[type="email"]', username)
+        await page.type('input[type="password"]', password)
+        await page.click('button[type="submit"]')
+        await page.waitForSelector('.checkin-chat')
+        await page.goto('https://app.crossover.com/x/dashboard/contractor/checkin-chat')
         //.wait('.highcharts-series-group')
-        .wait('checkin-metric-trend-popup')
-        .click('td[class="unknown clickable"]')
-        .click('span.ui-select-toggle')
+        await page.waitForSelector('checkin-metric-trend-popup')
+        await page.click('td[class="unknown clickable"]')
+        await page.click('span.ui-select-toggle')
         //.click('span.ui-select-choices-row-inner')
-        .click('div.ui-select-choices-row')
-        .type(generate_message(), 'textarea')
-        .wait(1000)
-        .click('button[type="submit"]')
-        .wait(10000)
-    await chromeless.end()
-}
-
-async function close() {
-    await console.error.bind(console)
-    //console.log("Clearing queue")
-    //chromeless.queue.comandQueue = {};
-    //chromeless.queue.lastWaitAll = null;
-    //chromeless.lastReturnPromise = null;
-    await chromeless.end()
-    console.log("Emergency close done")
+        await page.click('div.ui-select-choices-row')
+        await page.type('textarea', generate_message())
+        await page.waitFor(1000)
+        await page.click('button[type="submit"]')
+        console.log("All done")
+        await page.waitFor(10000)
+    } finally {
+        await browser.close()
+    }
 }
 
 function generate_message() {
     const lines = require('fs').readFileSync('reason_list.txt', 'utf-8').split(/\r?\n/)
-    const numLines = randomInt(1, 5)
+    const numLines = randomInt(2, 6)
     console.log("Num lines: " + numLines)
     const randomLines = getUniqueRandomArray(lines, { size: numLines });
     return randomLines.join("\n")
 }
 
 run()
-    .catch(() => close())
-    .catch(console.error.bind(console))
